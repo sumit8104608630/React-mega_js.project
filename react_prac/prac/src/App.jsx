8f8cc1e279589195384a8d0953e286076201466e
sumit sharma <sumit8104608630@gmail.com>
2024-08-08 17:30:46 +0530

    react project

--- react_prac/prac/src/App.jsx
+++ react_prac/prac/src/App.jsx
@@ -3,15 +3,42 @@ import './App.css'
 import UseCall from './UseCall'
 import UseCallBck from './UseCallBck'
 import UseRef from './UseRef'
-
+import {useNavigate,Outlet,NavLink} from "react-router-dom"
 function App() {
+  const navigate=useNavigate()
+  const navLinkStyle = ({ isActive }) => ({
+    textDecoration: 'none',
+    padding: '10px',
+    backgroundColor: isActive ? 'red' : 'transparent',
+    color: isActive ? 'white' : 'black'
+  })
+  const navItems = [
+    {
+      name: 'Home',
+      slug: "/",
+    }, 
+    {
+      name: "child",
+      slug: "/child",
+  },
+  {
+      name: "UseCall",
+      slug: "/UseCall",
+  },
+  {
+      name: "UseRef",
+      slug: "/UseRef",
+  },
 
+  ]
   return (
-    <>
-      <UseCall/>
-      <UseRef/>
-      <UseCallBck/>
-    </>
+    <div>
+      <nav ><ul className='f'>{navItems.map(item=><NavLink key={item.name} to={item.slug} style={navLinkStyle}><li >{item.name}</li></NavLink>)}</ul></nav>
+      <button onClick={()=>navigate("/child",{replace:true})}>Go to Child</button>
+      <button onClick={()=>navigate(-1)}>Go to Child</button>
+
+ <Outlet />
+ </div>
   )
 }
 
--- react_prac/prac/src/main.jsx
+++ react_prac/prac/src/main.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { createBrowserRouter,RouterProvider } from "react-router-dom"
+import Child from './Child.jsx'
+import UseCall from './UseCall.jsx'
+import UseRef from './UseRef.jsx'
+const router=createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children:[
+    {
+      path:'/child',
+      element:<Child/>
+    },
+  {
+    path:'/UseCall',
+    element:<UseCall/>
+  },
+  {
+    path:'/UseRef',
+    element:<UseRef/>
+  },
+  {
+    path:'/*',
+    element:(<>No  url Match found</>)
+  },
+]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
